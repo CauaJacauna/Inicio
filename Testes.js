1ef7b4408e42b63662c3e9b92fd4f23cb15804b7
Cauã Jacaúna <82884472+CauaJacauna@users.noreply.github.com>
2023-12-10 21:49:07 -0300

    Abrir no PHP

--- Testes.js
+++ Testes.js
@@ -17,3 +17,57 @@ function funcao() {
  funcao();
 
 //Fim da função ↑↑
+
+
+/*
+
+Claro, aqui está um exemplo simples de um formulário HTML e um script PHP para coletar Nome, Email e Idade:
+
+Formulário HTML (`index.html`):
+```html
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário PHP</title>
+</head>
+<body>
+    <form action="processar.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br>
+
+        <label for="idade">Idade:</label>
+        <input type="number" id="idade" name="idade" required><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
+```
+
+Script PHP (`processar.php`):
+```php
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Coletar dados do formulário
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $idade = $_POST["idade"];
+
+    // Exibir os dados coletados
+    echo "Nome: $nome<br>";
+    echo "Email: $email<br>";
+    echo "Idade: $idade anos";
+} else {
+    // Se o formulário não foi enviado corretamente
+    echo "Erro no envio do formulário.";
+}
+?>
+```
+
+Certifique-se de que os arquivos estejam no mesmo diretório e abra `index.html` em um navegador para testar o formulário PHP.
+
+*/
